3104df043f7502869357529d276aea7a755cbe5a
Kagapay <kagapj@gmail.com>
2025-07-24 03:22:24 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/jettas/',
+  base: "/jettas/",
   plugins: [react()],
-})
+});
