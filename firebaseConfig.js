07de8427a763de66236e800ac7390b79fdd12cb2
Kagapay <kagapj@gmail.com>
2025-07-24 01:26:06 +0100

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,12 @@
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDPW9WyXdQqcHvBaCe19aIPBIFSDl47gXk",
+  authDomain: "officialjjettas-b8533.firebaseapp.com",
+  projectId: "officialjjettas-b8533",
+  storageBucket: "officialjjettas-b8533.firebasestorage.app",
+  messagingSenderId: "719968450837",
+  appId: "1:719968450837:web:4b8b57be85ce3c7171911c",
+  measurementId: "G-C4SEDLBJC7"
 };
+
 export default firebaseConfig;
